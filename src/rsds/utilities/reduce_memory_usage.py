4cdae5f5fa89cae0875c40c579ea62dc3d89747d
Rory <rorysarten@gmail.com>
2022-07-04 22:08:19 +1200

    reformat to functional

--- src/rsds/utilities/reduce_memory_usage.py
+++ src/rsds/utilities/reduce_memory_usage.py
@@ -6,50 +6,52 @@ def reduce_memory_usage(dataframe: pd.DataFrame, verbose: bool = True) -> pd.Dat
 
     start_memory = dataframe.memory_usage().sum() / 1024**2
     if verbose:
-        print(f"Memory usage of dataframe is {start_memory} MB")
+        print(f"Memory usage of dataframe is {start_memory:.2f} MB")
 
     for col in dataframe.columns:
         col_type = dataframe[col].dtype
 
         if col_type != "object":
             if str(col_type)[:3] == "int":
-                mod_type_int(dataframe, col)
+                dataframe[col] = mod_type_int(dataframe[col])
             else:
-                mod_type_float(dataframe, col)
+                dataframe[col] = mod_type_float(dataframe[col])
         else:
             dataframe[col] = dataframe[col].astype("category")
 
     if verbose:
         end_memory = dataframe.memory_usage().sum() / 1024**2
-        print(f"Memory usage of dataframe after reduction {end_memory} MB")
-        print(f"Reduced by {100 * (start_memory - end_memory) / start_memory} % ")
+        print(f"Memory usage of dataframe after reduction {end_memory:.2f} MB")
+        print(f"Reduced by {(100 * (start_memory - end_memory) / start_memory):.2f} % ")
 
     return dataframe
 
 
-def mod_type_int(dataframe: pd.DataFrame, col: str) -> None:
-    """Modify column in-place if integer type"""
-    c_min = dataframe[col].min()
-    c_max = dataframe[col].max()
+def mod_type_int(col: pd.Series) -> pd.Series:
+    """Modify Series if integer type"""
+    c_min = col.min()
+    c_max = col.max()
 
     if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-        dataframe[col] = dataframe[col].astype(np.int8)
+        col = col.astype(np.int8)
     elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-        dataframe[col] = dataframe[col].astype(np.int16)
+        col = col.astype(np.int16)
     elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-        dataframe[col] = dataframe[col].astype(np.int32)
+        col = col.astype(np.int32)
     elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-        dataframe[col] = dataframe[col].astype(np.int64)
+        col = col.astype(np.int64)
+
+    return col
 
 
-def mod_type_float(dataframe: pd.DataFrame, col: str) -> None:
-    """Modify column in-place if float type"""
-    c_min = dataframe[col].min()
-    c_max = dataframe[col].max()
+def mod_type_float(col: pd.Series) -> pd.Series:
+    """Modify Series if float type"""
+    c_min = col.min()
+    c_max = col.max()
 
     if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-        dataframe[col] = dataframe[col].astype(np.float16)
+        col = col.astype(np.float16)
     elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-        dataframe[col] = dataframe[col].astype(np.float32)
-    else:
-        pass
+        col = col.astype(np.float32)
+
+    return col
